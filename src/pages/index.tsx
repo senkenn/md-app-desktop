0d66df87bad347c245e6f45bfac32cdfb6ad5ed1
senkenn <nayu3898@gmail.com>
2022-11-11 08:59:17 +0900

    Update css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import ReactMarkdown from 'react-markdown';
  * @returns {JSX.Element}
  */
 export default function App(): JSX.Element {
-  const [markdown, setMarkdown] = useState();
+  const [markdown, setMarkdown] = useState("");
 
   const setData = (e: any) => {
     e.preventDefault();
@@ -17,34 +17,28 @@ export default function App(): JSX.Element {
   };
 
   return (
-    <div className="post-form min-h-screen">
-      <div className="h-screen flex flex-col">
-        <div className="flex-grow flex-shrink">
-          <form className="h-full">
-            <div className="flex justify-between h-full">
+    <div className="h-screen flex">
+        <div className="flex-grow flex">
 
-              <div className="w-1/2">
-                <textarea
-                  name="md"
-                  id="md"
-                  placeholder="Markdownで記述"
-                  className="markdown-form resize-none w-full h-full border shadow-xl mb-5 rounded-xl focus:outline-none py-4 px-2"
-                  value={markdown}
-                  onChange={setData}
-                ></textarea>
-              </div>
+          <div className="w-1/2">
+            <textarea
+              name="md"
+              id="md"
+              placeholder="Markdownで記述"
+              className="markdown-form resize-none w-full h-full border shadow-xl mb-5 rounded-xl focus:outline-none py-4 px-2"
+              value={markdown}
+              onChange={setData}
+            ></textarea>
+          </div>
 
-              <div className="w-1/2">
-                <div className="markdown-preview h-full w-full border shadow-xl mb-5 rounded-xl py-4 px-2 bg-white">
-                  <ReactMarkdown unwrapDisallowed={false}>
-                    {markdown}
-                  </ReactMarkdown>
-                </div>
-              </div>
-
+          <div className="w-1/2">
+            <div className="markdown-preview h-full w-full border shadow-xl mb-5 rounded-xl py-4 px-2 bg-white">
+              <ReactMarkdown unwrapDisallowed={false}>
+                {markdown}
+              </ReactMarkdown>
             </div>
-          </form>
-        </div>
+          </div>
+
       </div>
     </div>
   );
