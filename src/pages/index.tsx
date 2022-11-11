7ce76d5679691967a03d76821bc7d4846b0c0c6d
senkenn <nayu3898@gmail.com>
2022-11-11 13:13:37 +0900

    Minor update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,9 @@ import ReactMarkdown from 'react-markdown';
  * @returns {JSX.Element}
  */
 export default function App(): JSX.Element {
-  const [markdown, setMarkdown] = useState("");
+  const [markdown, setMarkdown] = useState('');
 
-  const setData = (e: any) => {
+  const setData = (e: any): void => {
     e.preventDefault();
 
     setMarkdown(e.target.value);
@@ -18,26 +18,26 @@ export default function App(): JSX.Element {
 
   return (
     <div className="h-screen flex">
-        <div className="flex-grow flex">
+      <div className="grow flex">
 
-          <div className="w-1/2">
-            <textarea
-              name="md"
-              id="md"
-              placeholder="Markdownで記述"
-              className="markdown-form resize-none w-full h-full border shadow-xl mb-5 rounded-xl focus:outline-none py-4 px-2"
-              value={markdown}
-              onChange={setData}
-            ></textarea>
-          </div>
+        <div className="w-1/2">
+          <textarea
+            name="md"
+            id="md"
+            placeholder="Markdownで記述"
+            className="markdown-form resize-none w-full h-full border shadow-xl mb-5 rounded-xl focus:outline-none py-4 px-2"
+            value={markdown}
+            onChange={setData}
+          ></textarea>
+        </div>
 
-          <div className="w-1/2">
-            <div className="markdown-preview h-full w-full border shadow-xl mb-5 rounded-xl py-4 px-2 bg-white">
-              <ReactMarkdown unwrapDisallowed={false}>
-                {markdown}
-              </ReactMarkdown>
-            </div>
+        <div className="w-1/2">
+          <div className="markdown-preview h-full w-full border shadow-xl mb-5 rounded-xl py-4 px-2 bg-white">
+            <ReactMarkdown unwrapDisallowed={false}>
+              {markdown}
+            </ReactMarkdown>
           </div>
+        </div>
 
       </div>
     </div>
