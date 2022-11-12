4909c6d19fdc7a5d18c055af7b5548cfc2330fda
senkenn <nayu3898@gmail.com>
2022-11-12 11:45:15 +0900

    Update css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,28 +18,24 @@ export default function App(): JSX.Element {
 
   return (
     <div className="h-screen flex">
-      <div className="grow flex">
 
-        <div className="w-1/2">
-          <textarea
-            name="md"
-            id="md"
-            placeholder="Markdownで記述"
-            className="markdown-form resize-none w-full h-full border shadow-xl mb-5 rounded-xl focus:outline-none py-4 px-2"
-            value={markdown}
-            onChange={setData}
-          ></textarea>
-        </div>
-
-        <div className="w-1/2">
-          <div className="markdown-preview h-full w-full border shadow-xl mb-5 rounded-xl py-4 px-2 bg-white">
-            <ReactMarkdown unwrapDisallowed={false}>
-              {markdown}
-            </ReactMarkdown>
-          </div>
-        </div>
-
+      <div className="w-1/2">
+        <textarea
+          placeholder="Markdownで記述"
+          className="resize-none w-full h-full border rounded-xl focus:outline-none block"
+          value={markdown}
+          onChange={setData}
+        ></textarea>
       </div>
+
+      <div className="w-1/2">
+        <div className="h-full w-full border rounded-xl bg-white">
+          <ReactMarkdown unwrapDisallowed={false}>
+            {markdown}
+          </ReactMarkdown>
+        </div>
+      </div>
+
     </div>
   );
 }
